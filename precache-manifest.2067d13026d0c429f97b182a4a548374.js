862d15b0c16389f9e3c86a9ff00064a4e30c4101
RyFogle <13fogle@gmail.com>
2020-08-17 19:25:03 -0700

    Updates

---
+++ precache-manifest.2067d13026d0c429f97b182a4a548374.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d619985c98cbcd7b1ef30998869bb957",
+    "url": "/kitch/index.html"
+  },
+  {
+    "revision": "2b5f4233d2bd748cf989",
+    "url": "/kitch/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "804f16f016748851d4c9",
+    "url": "/kitch/static/js/2.57003d19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kitch/static/js/2.57003d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b5f4233d2bd748cf989",
+    "url": "/kitch/static/js/main.116beca9.chunk.js"
+  },
+  {
+    "revision": "051d35485ced3a5c728a",
+    "url": "/kitch/static/js/runtime-main.22e7a070.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kitch/static/media/logo.5d5d9eef.svg"
+  }
+]);
