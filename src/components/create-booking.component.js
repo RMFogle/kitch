d312d7e937257955869de1d55576559e624c6729
RyFogle <13fogle@gmail.com>
2020-09-08 20:43:07 -0700

    adding test for booking component

--- src/components/create-booking.component.js
+++ src/components/create-booking.component.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'; 
+import DatePicker from 'react-datepicker'; 
+import "react-datepicker/dist/react-datepicker.css"; 
 
 export default class CreateBooking extends Component {
     constructor(props) {
@@ -17,6 +19,13 @@ export default class CreateBooking extends Component {
             date: new Date(), 
             clients: []
         }
+    }
+
+    componentDidMount() {
+        this.setState({
+            clients: ['test client'], 
+            clientname: 'test client'
+        })
     }
 
     onChangeClientname(e) {
@@ -61,7 +70,57 @@ export default class CreateBooking extends Component {
     render() { 
         return (
             <div>
-                <p>You are on the Create Booking Component!</p>
+                <h3>Create Booking</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Client Name: </label>
+                        <select ref="clientInput" 
+                        required
+                        className="form-control"
+                        value={this.state.clientname}
+                        onChange={this.onChangeClientname}>
+                            {
+                                this.state.clients.map(function(client) {
+                                    return <option 
+                                        key={client}
+                                        value={client}>{client}
+                                        </option>; 
+                                })
+                            }
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Event: </label>
+                        <input type="text" 
+                        required 
+                        className="form-control"
+                        value={this.state.eventtype}
+                        onChange={this.onChangeEventtype}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Location: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.location}
+                        onChange={this.onChangeLocation}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Date: </label>
+                        <div>
+                            <DatePicker 
+                                selected={this.state.date}
+                                onChange={this.onChangeDate}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Save Booking" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
