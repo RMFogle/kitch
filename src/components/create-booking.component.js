0758483ca88944600d65f537f5394486616096e3
RyFogle <13fogle@gmail.com>
2020-09-08 20:15:25 -0700

    adding booking component with constructor

--- src/components/create-booking.component.js
+++ src/components/create-booking.component.js
@@ -1,6 +1,63 @@
 import React, { Component } from 'react'; 
 
 export default class CreateBooking extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeClientname = this.onChangeClientname.bind(this); 
+        this.onChangeEventtype = this.onChangeEventtype.bind(this); 
+        this.onChangeLocation = this.onChangeLocation.bind(this); 
+        this.onChangeDate = this.onChangeDate.bind(this); 
+        this.onSubmit = this.onSubmit.bind(this); 
+        
+        this.state = {
+            clientname: '', 
+            eventtype: '', 
+            location: '', 
+            date: new Date(), 
+            clients: []
+        }
+    }
+
+    onChangeClientname(e) {
+        this.setState({
+            clientname: e.target.value 
+        }); 
+    }
+
+    onChangeEventtype(e) {
+        this.setState({
+            eventtype: e.target.value 
+        }); 
+    }
+
+    onChangeLocation(e) {
+        this.setState({
+            location: e.target.value 
+        }); 
+    }
+
+    onChangeDate(date) {
+        this.setState({
+            date: date 
+        }); 
+    }
+
+    onSubmit(e) { 
+        e.preventDefault(); 
+
+        const booking = {
+            clientname: this.state.clientname, 
+            eventtype: this.state.eventtype, 
+            location: this.state.location, 
+            date: this.state.date 
+        }
+
+        console.log(booking); 
+
+        window.location = '/'; 
+    }
+    
     render() { 
         return (
             <div>
