9e1215f58a0168a4650f36746cfe0e61981464a4
RyFogle <13fogle@gmail.com>
2020-09-09 21:00:36 -0700

    adding client component and constructor

--- src/components/create-client.component.js
+++ src/components/create-client.component.js
@@ -1,10 +1,113 @@
 import React, { Component } from 'react'; 
 
 export default class CreateClient extends Component {
+    constructor(props) {
+        super(props); 
+
+        this.onChangeClientname = this.onChangeClientname.bind(this); 
+        this.onChangePhone = this.onChangePhone.bind(this); 
+        this.onChangeEmail = this.onChangeEmail.bind(this); 
+        this.onChangeNotes = this.onChangeNotes.bind(this); 
+        this.onSubmit = this.onSubmit.bind(this); 
+
+        this.state = {
+            clientname: '', 
+            phone: '', 
+            email: '', 
+            notes: '' 
+        }
+    }
+
+    onChangeClientname(e) {
+        this.setState({
+            clientname: e.target.value
+        }); 
+    }
+
+    onChangePhone(e) {
+        this.setState({
+            phone: e.target.value
+        }); 
+    }
+
+    onChangeEmail(e) {
+        this.setState({
+            email: e.target.value 
+        }); 
+    }
+
+    onChangeNotes(e) {
+        this.setState({
+            notes: e.target.value 
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault(); 
+
+        const client = {
+            clientname: this.state.clientname, 
+            phone: this.state.phone, 
+            email: this.state.email, 
+            notes: this.state.notes 
+        }
+
+        console.log(client); 
+
+        this.setState({
+            clientname: '', 
+            phone: '', 
+            email: '', 
+            notes: ''
+        })
+    }
+
+
     render() { 
         return (
             <div>
-                <p>You are on the Create Client Component!</p>
+                <h3>New Client</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Client: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.clientname}
+                        onChange={this.onChangeClientname}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Phone: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.phone}
+                        onChange={this.onChangePhone}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Email: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.email}
+                        onChange={this.onChangeEmail}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Notes: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.notes}
+                        onChange={this.onChangeNotes}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Add Client" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -16,7 +16,7 @@ export default class Navbar extends Component {
                     <Link to="/create" className="nav-link">Create Booking</Link>
                     </li>
                     <li className="navbar-item">
-                    <Link to="/client" className="nav-link">Add Client</Link>
+                    <Link to="/client" className="nav-link">Clients</Link>
                     </li>
               </ul>
               </div>
