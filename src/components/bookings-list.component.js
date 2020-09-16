912d390194caf56065c478f6c899305a30253783
RyFogle <13fogle@gmail.com>
2020-09-15 19:59:05 -0700

    adding bookings list get and delete

--- src/components/bookings-list.component.js
+++ src/components/bookings-list.component.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react'; 
+import { Link } from 'react-router-dom'; 
+import axios from 'axios'; 
+
 
 export default class BookingsList extends Component {
+    constructor(props) { 
+        super(props); 
+
+ this.deleteBooking = this.deleteBooking.bind(this); 
+
+ this.state = {bookings: []}; 
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/bookings/')
+        .then(response => {
+            this.setState({ bookings: response.data})
+        })
+        .catch((error) => {
+            console.log(error); 
+        })
+    }
+
+    deleteBooking(id) {
+        axios.delete('http://localhost:5000/bookings/' + id)
+            .then(res => console.log(res.data)); 
+            
+        this.setState({
+            bookings: this.state.bookings.filter(el => el.id !== id)
+        })
+    }
+
     render() { 
         return (
             <div>
