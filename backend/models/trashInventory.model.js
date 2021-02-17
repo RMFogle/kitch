ceb91862db1badd9043fb913799ba94557020030
RyFogle <13fogle@gmail.com>
2021-02-16 20:31:33 -0800

    adding trash inventory model

---
+++ backend/models/trashInventory.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema; 
+
+const trashInventorySchema = new Schema({
+    fooditem: { type: String, required: true }, 
+    category: { type: String, required: true }, 
+    unitsize: { type: String, required: true }, 
+    instock: { type: Number, required: true }, 
+    needed: { type: Number, required: true }, 
+    topurchase: { type: Number, required: true }, 
+    unitprice: { type: Number, required: true }, 
+    totalcost: { type: Number, required: true},
+}, {
+    timestamps: true, 
+}); 
+
+const TrashInventory = mongoose.model('TrashInventory', trashInventorySchema)
+
+module.exports = TrashInventory;
