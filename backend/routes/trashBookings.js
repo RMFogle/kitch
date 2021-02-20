039661cb99695efa2ead5ba4e0fa112ae46d783a
RyFogle <13fogle@gmail.com>
2021-02-20 11:18:25 -0800

    add routes for trash booking

---
+++ backend/routes/trashBookings.js
@@ -0,0 +1,58 @@
+const router = require('express').Router(); 
+let TrashBooking = require('../models/trashBooking.model'); 
+
+router.route('/').get((req, res) => {
+    TrashBooking.find()
+    .then(trashBookings => res.json(trashBookings))
+    .catch(err => res.status(400).json('Error: ' + err)); 
+});
+
+router.route('/add').post((req, res) => {
+    const clientname = req.body.clientname; 
+    const eventtype = req.body.eventtype; 
+    const location = req.body.location; 
+    const date = Date.parse(req.body.date); 
+   // const time = TimeRanges.parse(req.body.time); 
+
+    
+const newTrashBooking = new TrashBooking({
+    clientname, 
+    eventtype, 
+    location, 
+    date, 
+   // time, 
+}); 
+
+newTrashBooking.save()
+.then(() => res.json('Trash Booking added!'))
+.catch(err => res.status(400).json('Error: ' + err)); 
+}); 
+
+router.route('/:id').get((req, res) => {
+    TrashBooking.findById(req.params.id)
+        .then(trashBooking => res.json(trashBooking))
+        .catch(err => res.status(400).json('Error: ' + err)); 
+});
+
+router.route('/:id').delete((req, res) => {
+    TrashBooking.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Trash Booking deleted'))
+        .catch(err => res.status(400).json('Error: ' + err)); 
+}); 
+
+router.route('/update/:id').post((req, res) => {
+    TrashBooking.findById(req.params.id)
+        .then(trashBooking => {
+            trashBooking.clientname = req.body.clientname; 
+            trashBooking.eventtype = req.body.eventtype; 
+            trashBooking.location = req.body.location; 
+            trashBooking.date = Date.parse(req.body.date); 
+
+            trashBooking.save()
+                .then(() => res.json('Trash Booking updated!'))
+                .catch(err => res.status(400).json('Error: ' + err)); 
+        }) 
+        .catch(err => res.status(400).json('Error: ' + err)); 
+}); 
+
+module.exports = router; 
