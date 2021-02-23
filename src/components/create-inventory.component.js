97dca4a9e23216280e7a02a91087dab22de48659
RyFogle <13fogle@gmail.com>
2021-02-22 20:35:04 -0800

    adding component for create inventory

---
+++ src/components/create-inventory.component.js
@@ -0,0 +1,193 @@
+import React, { Component } from 'react'; 
+import axios from 'axios'; 
+
+export default class CreateInventory extends Component {
+    constructor(props) {
+        super(props); 
+
+        this.onChangeFooditem = this.onChangeFooditem.bind(this); 
+        this.onChangeCategory = this.onChangeCategory.bind(this); 
+        this.onChangeUnitsize = this.onChangeUnitsize.bind(this); 
+        this.onChangeInstock = this.onChangeInstock.bind(this); 
+        this.onChangeNeeded = this.onChangeNeeded.bind(this); 
+        this.onChangeTopurchase = this.onChangeTopurchase.bind(this); 
+        this.onChangeUnitprice = this.onChangeUnitprice.bind(this); 
+        this.onChangeTotalcost = this.onChangeTotalcost.bind(this); 
+
+        this.state = {
+            fooditem: '', 
+            category: '', 
+            unitsize: '', 
+            instock: '', 
+            needed: '', 
+            topurchase: '', 
+            unitprice: '', 
+            totalcost: '' 
+        }   
+    }
+
+    onChangeFooditem(e) {
+        this.setState({
+            fooditem: e.target.value 
+        }); 
+    }
+
+    onChangeCategory(e) {
+        this.setState({
+            category: e.target.value 
+        }); 
+    }
+
+    onChangeUnitsize(e) {
+        this.setState({
+            unitsize: e.target.value 
+        }); 
+    }
+
+    onChangeInstock(e) {
+        this.setState({
+            instock: e.target.value 
+        }); 
+    }
+
+    onChangeNeeded(e) {
+        this.setState({
+            needed: e.target.value 
+        }); 
+    }
+
+    onChangeTopurchase(e) {
+        this.setState({
+            topurchase: e.target.value 
+        }); 
+    }
+
+    onChangeUnitprice(e) {
+        this.setState({
+            unitprice: e.target.value 
+        }); 
+    }
+
+    onChangeTotalcost(e) {
+        this.setState({
+            totalcost: e.target.value 
+        }); 
+    }
+
+    onSubmit(e) {
+        alert("Inventory Successfully Added!!!")
+        e.preventDefault(); 
+
+        const inventory = {
+            fooditem: this.state.fooditem, 
+            category: this.state.category, 
+            unitsize: this.state.unitsize,
+            instock: this.state.instock,
+            needed: this.state.needed, 
+            topurchase: this.state.topurchase, 
+            unitprice: this.state.unitprice, 
+            totalcost: this.state.totalcost
+        }
+
+        console.log(inventory); 
+
+        axios.post('http://localhost:5000/inventorys/add', inventory)
+            .then(res => console.log(res.data)); 
+
+        this.setState({
+            fooditem: '', 
+            category: '', 
+            unitsize: '', 
+            instock: '', 
+            needed: '', 
+            topurchase: '', 
+            unitprice: '', 
+            totalcost: ''
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Add Inventory</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Food Item: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.fooditem}
+                        onChange={this.onChangeFooditem}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Category: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.category}
+                        onChange={this.onChangeCategory}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Unit Size: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.unitsize}
+                        onChange={this.onChangeUnitsize}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>In Stock: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.instock}
+                        onChange={this.onChangeInstock}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Need: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.needed}
+                        onChange={this.onChangeNeeded}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>To Purchase: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.topurchase}
+                        onChange={this.onChangeTopurchase}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Unit Price: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.unitprice}
+                        onChange={this.onChangeUnitprice}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Total Cost: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.totalcost}
+                        onChange={this.onChangeTotalcost}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Add Inventory" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
