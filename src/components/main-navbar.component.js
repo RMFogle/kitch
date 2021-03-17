ca96e3574a2dce8d973af7f9724fcd433375534c
RyFogle <13fogle@gmail.com>
2021-03-16 23:05:03 -0700

    adding non working search bar

--- src/components/main-navbar.component.js
+++ src/components/main-navbar.component.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown'; 
+import '../styles/style.css'; 
 
 export default class MainNavbar extends Component {
 
     render() {
         return (
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="lg">
                 <Navbar.Brand href="/home">kitch</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="/inventory" activeClassName="selectedLink">Inventory</Nav.Link>
+                        <Nav.Link href="/inventory">Inventory</Nav.Link>
                         <Nav.Link href="/booking">Bookings</Nav.Link>
                         <Nav.Link href="/client">Clients</Nav.Link>
                             <NavDropdown title="Other" id="collasible-nav-dropdown">
