0de558224b8577372be638d5fb00c6322094a0e5
RyFogle <13fogle@gmail.com>
2021-04-27 21:08:45 -0700

    adding respons and styling

--- src/components/home/homepage.component.js
+++ src/components/home/homepage.component.js
@@ -3,6 +3,9 @@ import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image'; 
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container'; 
+import Row from 'react-bootstrap/Row'; 
+import Col from 'react-bootstrap/Col'; 
 import { Icon } from '@iconify/react';
 import listOutline from '@iconify-icons/eva/list-outline';
 import peopleGroup from '@iconify-icons/vs/people-group';
@@ -32,173 +35,170 @@ export default class HomePage extends Component {
         return (
             <div className="container-main">
                 <div className="container-carousel">
-                <div className="row">
-                    <div className="col">
-            <Carousel className="carousel">
-                <Carousel.Item interval={3000}>
-                <img
-                className="d-block w-100"
-                src={KitchCarousel}
-                alt="First slide"
-                />
-            </Carousel.Item>
-                <Carousel.Item interval={2000}>
-                <img
-                className="d-block w-100"
-                src={InventoryCarousel}
-                alt="Second slide"
-                />
-            </Carousel.Item>
-                <Carousel.Item interval={2000}>
-                <img
-                className="d-block w-100"
-                src={ClientsCarousel}
-                alt="Third slide"
-                />
-            </Carousel.Item>
-                <Carousel.Item interval={2000}>
-                <img
-                className="d-block w-100"
-                src={BookingCarousel}
-                alt="Fourth slide"
-                />
-            </Carousel.Item>
-                <Carousel.Item interval={2000}>
-                <img
-                className="d-block w-100"
-                src={ReportsCarousel}
-                alt="Fifth slide"
-                />
-            </Carousel.Item>
-        </Carousel>
-        </div>
-        </div>
+                    <div className="row">
+                        <div className="col">
+                <Carousel className="carousel">
+                    <Carousel.Item interval={3000}>
+                        <img
+                        className="d-block w-100"
+                        src={KitchCarousel}
+                        alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item interval={2000}>
+                        <img
+                        className="d-block w-100"
+                        src={InventoryCarousel}
+                        alt="Second slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item interval={2000}>
+                        <img
+                        className="d-block w-100"
+                        src={ClientsCarousel}
+                        alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item interval={2000}>
+                        <img
+                        className="d-block w-100"
+                        src={BookingCarousel}
+                        alt="Fourth slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item interval={2000}>
+                        <img
+                        className="d-block w-100"
+                        src={ReportsCarousel}
+                        alt="Fifth slide"
+                        />
+                    </Carousel.Item>
+                </Carousel>
+                </div>
+            </div>
         </div>
         <br>
         </br>
         <div className="container-quote">
             <div className="row">
                 <div className="col">
-        <Card>
-    <Card.Header></Card.Header>
-            <Image src={Quote} fluid className="info-image" />
-    </Card>
-    </div>
-    </div>
-    </div>
-        <br>
-        </br>
-        <div className="container-info">
-        <div className="row">
-            <div className="col-6">
-        <Card className="card-info" id="how">
-            <Card.Header><Icon icon={listOutline} /></Card.Header>
-                <Card.Body>
-                <Card.Title>Manage Your Inventory</Card.Title>
-                    <Card.Text>
-                        Add food items and kitchen supplies. Keep track of total costs and stock. 
-                    </Card.Text>
-                </Card.Body>
-        </Card>
-        </div>
-            <div className="col-6">
-        <Image src={InventoryScreenshot} fluid className="info-image" />
-        </div>
-        </div>
+                    <Card>
+                        <Image src={Quote} fluid className="info-image" />
+                    </Card>
+                </div>
+            </div>
         </div>
         <br>
         </br>
-        <div className="container-info">
-        <div className="row">
-            <div className="col-6">
-            <Image src={ClientScreenshot} fluid className="info-image" />
-        </div>
-            <div className="col-6">
-        <Card className="card-info">
-            <Card.Header><Icon icon={peopleGroup} /></Card.Header>
-                <Card.Body>
-                <Card.Title>Keep Track Of Your Clients</Card.Title>
-                    <Card.Text>
-                        Sortable tables make it easy to find a client and update their contact information. 
-                    </Card.Text>
-                </Card.Body>
-        </Card>
-        </div>
-        </div>
-        </div>
+        <Container className="container-info" fluid>
+            <Row>
+                <Col md={12} lg={6}>
+                    <Card className="card-info" id="how">
+                        <Card.Header><Icon icon={listOutline} /></Card.Header>
+                        <Card.Body>
+                        <Card.Title>Manage Your Inventory</Card.Title>
+                            <Card.Text>
+                            Add food items and kitchen supplies. Keep track of total costs and stock. 
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            <Col md={12} lg={6}>
+                <Image src={InventoryScreenshot} fluid className="info-image" />
+            </Col>
+            </Row>
+        </Container>
         <br>
         </br>
-        <div className="container-info">
-        <div className="row">
-            <div className="col-6">
-        <Card className="card-info">
-            <Card.Header><Icon icon={calendarClockOutline} /></Card.Header>
-                <Card.Body>
-                <Card.Title>Schedule Bookings</Card.Title>
-                    <Card.Text>
-                        Know when, where and how many guests you need to feed for your clients next event. 
-                    </Card.Text>
-                </Card.Body>
-        </Card>
-        </div>
-            <div className="col-6">
-        <Image src={BookingScreenshot} fluid className="info-image" />
-        </div>
-        </div>
-        </div>
+        <Container className="container-info" fluid>
+            <Row>
+                <Col md={12} lg={6} className="img-second">
+                    <Image src={ClientScreenshot} fluid className="info-image" />
+                </Col>
+                <Col md={12} lg={6}>
+                    <Card className="card-info">
+                        <Card.Header><Icon icon={peopleGroup} /></Card.Header>
+                        <Card.Body>
+                        <Card.Title>Keep Track Of Your Clients</Card.Title>
+                            <Card.Text>
+                            Sortable tables make it easy to find a client and update their contact information. 
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
         <br>
         </br>
-        <div className="container-info">
-        <div className="row">
-            <div className="col-6">
-            <Image src={ReportScreenshot} fluid className="info-image" />
-        </div>
-            <div className="col-6">
-        <Card className="card-info">
-            <Card.Header><Icon icon={barGraph} /></Card.Header>
-                <Card.Body>
-                <Card.Title>Generate Reports and Data Charts</Card.Title>
-                    <Card.Text>
+        <Container className="container-info" fluid>
+            <Row>
+                <Col md={12} lg={6}>
+                    <Card className="card-info">
+                        <Card.Header><Icon icon={calendarClockOutline} /></Card.Header>
+                        <Card.Body>
+                        <Card.Title>Schedule Bookings</Card.Title>
+                            <Card.Text>
+                            Know when, where and how many guests you need to feed for your clients next event. 
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            <Col md={12} lg={6}>
+                <Image src={BookingScreenshot} fluid className="info-image" />
+            </Col>
+            </Row>
+        </Container>
+        <br>
+        </br>
+        <Container className="container-info" fluid>
+            <Row>
+                <Col md={12} lg={6} className="img-second">
+                    <Image src={ReportScreenshot} fluid className="info-image" />
+                </Col>
+            <Col md={12} lg={6}>
+                <Card className="card-info">
+                    <Card.Header><Icon icon={barGraph} /></Card.Header>
+                    <Card.Body>
+                    <Card.Title>Generate Reports and Data Charts</Card.Title>
+                        <Card.Text>
                         Visulized data shows you how much you spend on inventory each month. 
                         How many new clients you've booked, or how many bookings you've cancelled per month. 
                         Make informed decisions about your business with our data charts powered by mongoDB Charts. 
-                    </Card.Text>
-                </Card.Body>
-        </Card>
-        </div>
-        </div>
-        </div>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+            </Row>
+        </Container>
         <br>
         </br>
         <div className="container-demo">
-            <Card className="card1">
+            <Card className="card-demo">
                     <Card.Img variant="top" src={FreeDemo} />
                         <Card.Body>
-                        <div className="row">
-                            <div className="col-4">
-                            <Button href="/demo" variant="info" size="lg">FREE Demo</Button>
+                            <div className="row">
+                                <div className="col-md-4">
+                                <Button href="/demo" variant="info" size="lg">FREE Demo</Button>
+                                </div>
+                            <br>
+                            </br>
+                                <div className="col-md-8">
+                                    <CreateScheduleDemo />
+                                </div>
                             </div>
-                            <br></br>
-                            <div className="col-8">
-                            <CreateScheduleDemo />
-                        </div>
-                        </div>
                         </Card.Body>
                     </Card>
-                    <br></br>
-                <Card className="text-center" id="card2">
-                    {/* <Card.Header></Card.Header> */}
-                    <Card.Body>
+                    <br>
+                    </br>
+                    <Card className="text-center" id="card-signup">
+                        <Card.Body>
                         <Card.Title><h4>Manage Your Catering Business Today!</h4></Card.Title>
-                        <Card.Text>See which plan is right for you.</Card.Text>
-                        <Button href="/pricing" variant="success" size="lg">Sign up</Button>
-                    </Card.Body>
-                    {/* <Card.Footer className="text-muted"></Card.Footer> */}
-                </Card>
-                {/* break */}
-                <br></br>
-        </div>
-        </div>
+                            <Card.Text>See which plan is right for you.</Card.Text>
+                                <Button href="/pricing" variant="success" size="lg">Sign up</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
         )
     }
 }
